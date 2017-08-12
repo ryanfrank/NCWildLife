9bc03fccacdae3fe97db1e82c6ca4f22e079f255
Ryan Frank <ryan_w_frank@mac.com>
2017-08-12 19:46:35 -0400

    Another test

--- application/views/rehab_main.php
+++ application/views/rehab_main.php
@@ -15,11 +15,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('application/bootstrap/css/bootstrap.min.css');?>">
 
 </head>
-<div class="row">
-    <div class="col-lg-9">
-        In the main DIV
-        <div class="col-8 col-sm-8">
-            Inside small DIV
+<div class="container-fluid">
+    In a container now... 
+    <div class="row">
+        <div class="col-lg-9">
+            In the main DIV
+            <div class="col-8 col-sm-8">
+                Inside small DIV
+            </div>
         </div>
     </div>
 </div>
