427b24bd065556f8b99423ba2b6580372285d8b4
Ryan Frank <ryan_w_frank@mac.com>
2017-08-12 19:43:03 -0400

    test grid

--- application/views/rehab_main.php
+++ application/views/rehab_main.php
@@ -15,45 +15,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('application/bootstrap/css/bootstrap.min.css');?>">
 
 </head>
-<div class="container">
-    <div class="row justify-content-start">
-        <div class="col-4">
-            One of two columns
-        </div>
-        <div class="col-4">
-            One of two columns
-        </div>
-    </div>
-    <div class="row justify-content-center">
-        <div class="col-4">
-            One of two columns
-        </div>
-        <div class="col-4">
-            One of two columns
-        </div>
-    </div>
-    <div class="row justify-content-end">
-        <div class="col-4">
-            One of two columns
-        </div>
-        <div class="col-4">
-            One of two columns
-        </div>
-    </div>
-    <div class="row justify-content-around">
-        <div class="col-4">
-            One of two columns
-        </div>
-        <div class="col-4">
-            One of two columns
-        </div>
-    </div>
-    <div class="row justify-content-between">
-        <div class="col-4">
-            One of two columns
-        </div>
-        <div class="col-4">
-            One of two columns
+<div class="row">
+    <div class="col-lg-9">
+        In the main DIV
+        <div class="col-8 col-sm-8">
+            Inside small DIV
         </div>
     </div>
 </div>
