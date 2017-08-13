b9957df5f85881ff3f5f98ef44141abaa227b33c
Ryan Frank <ryan_w_frank@mac.com>
2017-08-13 15:35:05 -0400

    Added jQuery

--- application/views/rehab_main.php
+++ application/views/rehab_main.php
@@ -66,6 +66,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </div>
     </div>
 </div>
-<div class="container-fluid mt-md-2 border border-dark rounded" id="content" style="min-height: 500px;"></div>
+<div class="container-fluid mt-md-2 ml-2 border border-dark rounded" id="content" style="min-height: 500px;"></div>
 </body>
 </html>
