658981e839c2854d0b59c53f2f18a9569038b9ef
Ryan Frank <ryan_w_frank@mac.com>
2017-08-13 14:29:04 -0400

    Added jQuery

--- application/views/intake_add.php
+++ application/views/intake_add.php
@@ -10,7 +10,7 @@
 
 <form class="mt-3">
     <div class="form-group row">
-        <label for="speciesSelection" class="col-sm-2">Species</label>
+        <label for="speciesSelection" class="col-sm-1">Species</label>
         <select class="form-control col-sm-3" id="speciesSelection">
             <?php
                 foreach ($species->result() as $row)
