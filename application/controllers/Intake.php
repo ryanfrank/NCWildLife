164d39be8417edd405a68e71766d14fd42a1ebf5
Ryan Frank <ryan_w_frank@mac.com>
2017-08-13 11:14:07 -0400

    Added jQuery

---
+++ application/controllers/Intake.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ryan_w_frank
+ * Date: 8/13/17
+ * Time: 10:51 AM
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Intake extends CI_Controller {
+
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     * 		http://example.com/index.php/welcome
+     *	- or -
+     * 		http://example.com/index.php/welcome/index
+     *	- or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     * @see https://codeigniter.com/user_guide/general/urls.html
+     */
+    public function index()
+    {
+        $this->load->view('intake_main');
+    }
+}
