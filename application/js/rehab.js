44b9979b2dc484b9633833fcb57014144f972898
Ryan Frank <ryan_w_frank@mac.com>
2017-08-13 17:59:19 -0400

    Added jQuery

---
+++ application/js/rehab.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    //$("mySubmit").click(function(event) {
+    $("goodSamaritanForm").submit(function(event) {
+        alert("intake submit / rehab.js");
+        event.preventDefault();
+        var firstName = $("input#firstName").val();
+        var lastName = $("input#lastName").val();
+        jQuery.ajax({
+            type: "POST",
+            url: "<?php echo base_url(); ?>" + "/Intake/addSamaritan",
+            dataType: 'json',
+            data: {first: firstName, last: lastName},
+            success: function(res) {
+                if (res)
+                {
+// Show Entered Value
+                    jQuery("div#content").show();
+                    jQuery("div#content").html(res.first);
+                    jQuery("div#value_pwd").html(res.last);
+                }
+            }
+        });
+    });
+});
