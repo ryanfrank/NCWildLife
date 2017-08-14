06330fbf7b0a1ad2f045c50b6a09a3b2324b11b3
Ryan Frank <ryan_w_frank@mac.com>
2017-08-14 17:55:57 -0400

    Added jQuery

--- application/views/admin_add_rehabber.php
+++ application/views/admin_add_rehabber.php
@@ -35,15 +35,15 @@
                     //$("myStatus").show();
                     $("#myStatus").show();
                     if (res === "success") {
-                        jQuery("div#myStatus").html('<div class="row"><div class="alert alert-success mt-lg-4 col-8 alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close">Successfully added ' + $("input#firstName").val() + ' ' + $("input#lastName").val() + '</button></div></div>');
+                        jQuery("div#myStatus").html('<div class="alert alert-success mt-lg-4 col-12 alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"></button>Successfully added ' + $("input#firstName").val() + ' ' + $("input#lastName").val() + '</div>');
                         document.getElementById("addRehabber").reset();
                     }
                     else if (res === "duplicate") {
-                        jQuery("div#myStatus").html('<div class="row"><div class="alert alert-warning mt-lg-4 col-8 alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close">Duplicate entry for ' + $("input#firstName").val() + ' ' + $("input#lastName").val() + '</button></div></div>');
+                        jQuery("div#myStatus").html('<div class="alert alert-warning mt-lg-4 col-12 alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"></button>Duplicate entry for ' + $("input#firstName").val() + ' ' + $("input#lastName").val() + '</div>');
                         document.getElementById("addRehabber").reset();
                     }
                     else {
-                        jQuery("div#myStatus").html('<div class="row"><div class="alert alert-danger mt-lg-4 col-8 alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close">Failed to add ' + $("input#firstName").val() + ' ' + $("input#lastName").val() + '</button></div></div>');
+                        jQuery("div#myStatus").html('<div class="alert alert-danger mt-lg-4 col-12 alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"></button>Failed to add ' + $("input#firstName").val() + ' ' + $("input#lastName").val() + '</div>');
                         document.getElementById("addRehabber").reset();
                     }
                 }
