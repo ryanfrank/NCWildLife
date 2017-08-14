26e3e9fe1dd21c4f2c2f61b59afcf997e76be5d9
Ryan Frank <ryan_w_frank@mac.com>
2017-08-14 17:25:38 -0400

    Added jQuery

--- application/views/rehab_main.php
+++ application/views/rehab_main.php
@@ -71,7 +71,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </div>
     </div>
 </div>
-<div class="row" hidden id="myStatus"></div>
+<div class="row" id="myStatus"></div>
 <div class="container-fluid mt-md-2 ml-2 border border-dark rounded" id="content" style="min-height: 500px; width: 98%">
     <div class="card-deck mt-3 mb-3">
         <div class="card col-4" style="width: 20rem;">
