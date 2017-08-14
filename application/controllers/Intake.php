2ae3fadc2efc5ea764e3a68572557a135cb5b060
Ryan Frank <ryan_w_frank@mac.com>
2017-08-14 13:07:36 -0400

    Added jQuery

--- application/controllers/Intake.php
+++ application/controllers/Intake.php
@@ -69,7 +69,7 @@ class Intake extends CI_Controller {
                 'good_samaritan_list'               => $data['emailList']
             );
             //$value = $data['firstName'];
-            $sql = "INSERT INTO good_samaritan ('good_samaritan_first_name','good_samaritan_last_name') values ('" . $data['firstName'] . "','" . $data['lastName'] . "'";
+            $sql = "INSERT INTO good_samaritan ('good_samaritan_first_name','good_samaritan_last_name') values ('crap','morecrap')";
             //$sql = $this->db->set($myData)->get_compiled_insert('good_samaritan');
 
             //$this->db->insert('good_samaritan', $myData);
