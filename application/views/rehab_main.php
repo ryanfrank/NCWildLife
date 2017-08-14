f2558e80a652fc1ca9f6f9e6bd539ab2cb1ccd40
Ryan Frank <ryan_w_frank@mac.com>
2017-08-14 15:25:34 -0400

    Added jQuery

--- application/views/rehab_main.php
+++ application/views/rehab_main.php
@@ -61,6 +61,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </div>
     </div>
 </div>
-<div class="container-fluid mt-md-2 ml-2 border border-dark rounded" id="content" style="min-height: 500px; width: 98%"></div>
+<div class="container-fluid mt-md-2 ml-2 border border-dark rounded" id="content" style="min-height: 500px; width: 98%">
+    <div class="card col-4" style="width: 20rem;">
+        <img class="card-img-top" src="<?php echo base_url();?>/application/images/squirrel_1.jpg" alt="Card image cap">
+        <div class="card-body">
+            <h4 class="card-title">Rehabilitation</h4>
+            <p class="card-text">Taking every opportunity to return wild life to their natural habitats.</p>
+        </div>
+    </div>
+    <div class="card col-4" style="width: 20rem;">
+        <img class="card-img-top" src="<?php echo base_url();?>/application/images/possum.jpg" alt="Card image cap">
+        <div class="card-body">
+            <h4 class="card-title">Education</h4>
+            <p class="card-text">Educating the public on the benefits of wild life and the role they play in our ecosystem.</p>
+        </div>
+    </div>
+    <div class="card col-4" style="width: 20rem;">
+        <img class="card-img-top" src="<?php echo base_url();?>/application/images/possum.jpg" alt="Card image cap">
+        <div class="card-body">
+            <h4 class="card-title">Partnership</h4>
+            <p class="card-text">Working with our peer partners to facilitate support systems and enhance capabilities by sharing knowledge and experience.</p>
+        </div>
+    </div>
+</div>
 </body>
 </html>
