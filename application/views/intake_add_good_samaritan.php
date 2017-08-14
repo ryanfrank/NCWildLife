eba6c1ee3750711adfa62bfd3e40d8124e5e9f17
Ryan Frank <ryan_w_frank@mac.com>
2017-08-14 15:04:46 -0400

    Added jQuery

--- application/views/intake_add_good_samaritan.php
+++ application/views/intake_add_good_samaritan.php
@@ -121,6 +121,6 @@
     </div>
     <br>
     <div class="row ml-1 mt-4">
-        <input type="submit" id="submitButton">
+        <input class="btn rounded" type="submit" id="submitButton">
     </div>
 </form>
