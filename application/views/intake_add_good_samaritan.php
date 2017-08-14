e26caea32e02f68e217b8bc55fb7bf1c0c5d5fdc
Ryan Frank <ryan_w_frank@mac.com>
2017-08-13 22:13:28 -0400

    Added jQuery

--- application/views/intake_add_good_samaritan.php
+++ application/views/intake_add_good_samaritan.php
@@ -98,7 +98,7 @@
     <div class="row">
         <div class="form-check col-3">
             <label class="form-check-label">
-                <input class="form-check-input" id="donationReceived" type="checkbox" value="1">
+                <input class="form-check-input" id="donationReceived" type="checkbox">
                     Donation received?
             </label>
         </div>
@@ -109,7 +109,7 @@
         </div>
         <div class="form-check col-3">
             <label class="form-check-label">
-                <input class="form-check-input" id="emailList" type="checkbox" value="1">
+                <input class="form-check-input" id="emailList" type="checkbox">
                 Would you like to be on our mailing list?
             </label>
         </div>
