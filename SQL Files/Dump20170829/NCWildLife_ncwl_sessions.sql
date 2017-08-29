18ffd436b07c9713d05e3b4c9c25a6d77323e7df
Ryan Frank <ryan_w_frank@mac.com>
2017-08-29 10:47:45 -0400

    Added Sessions configuration and uploaded DB Dump SQL from 08/29/2017.

---
+++ SQL Files/Dump20170829/NCWildLife_ncwl_sessions.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+--
+-- Host: localhost    Database: NCWildLife
+-- ------------------------------------------------------
+-- Server version	5.6.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ncwl_sessions`
+--
+
+DROP TABLE IF EXISTS `ncwl_sessions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ncwl_sessions` (
+  `id` varchar(128) NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `timestamp` int(10) unsigned NOT NULL DEFAULT '0',
+  `data` blob NOT NULL,
+  KEY `ncwl_sessions_timestamp` (`timestamp`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ncwl_sessions`
+--
+
+LOCK TABLES `ncwl_sessions` WRITE;
+/*!40000 ALTER TABLE `ncwl_sessions` DISABLE KEYS */;
+INSERT INTO `ncwl_sessions` VALUES ('e7ba11f93f33b4bb51e7dc6a182e1ba66b508526','::1',1504017799,'__ci_last_regenerate|i:1504017799;');
+/*!40000 ALTER TABLE `ncwl_sessions` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-08-29 10:46:29
---
+++ SQL Files/Dump20170829/NCWildLife_rehabber.sql
@@ -0,0 +1,64 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+--
+-- Host: nc28208-master    Database: NCWildLife
+-- ------------------------------------------------------
+-- Server version	5.5.52-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `rehabber`
+--
+
+DROP TABLE IF EXISTS `rehabber`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rehabber` (
+  `rehabber_id` int(5) NOT NULL AUTO_INCREMENT,
+  `rehabber_first_name` varchar(45) NOT NULL,
+  `rehabber_last_name` varchar(45) NOT NULL,
+  `rehabber_street` varchar(45) NOT NULL,
+  `rehabber_city` varchar(45) NOT NULL,
+  `rehabber_state` int(2) NOT NULL,
+  `rehabber_zip` int(9) NOT NULL,
+  `rehabber_email` varchar(45) NOT NULL,
+  `rehabber_phone` varchar(45) NOT NULL,
+  `rehabber_license` int(1) NOT NULL,
+  `created_date` datetime NOT NULL,
+  `updated_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `rehabber_volunteer` int(1) NOT NULL,
+  PRIMARY KEY (`rehabber_id`),
+  KEY `state_idx` (`rehabber_state`),
+  CONSTRAINT `state` FOREIGN KEY (`rehabber_state`) REFERENCES `states` (`state_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rehabber`
+--
+
+LOCK TABLES `rehabber` WRITE;
+/*!40000 ALTER TABLE `rehabber` DISABLE KEYS */;
+INSERT INTO `rehabber` VALUES (1,'Ryan','Frank','429 Aderholdt Road','Lincolnton',33,28092,'ryan_w_frank@mac.com','7048404309',0,'2017-08-14 16:14:56','2017-08-14 21:14:56',0),(2,'Emilie','Nelson','429 Aderholdt Road','Lincolnton',33,28092,'emilie.nelson@gmail.com','7048584372',1,'2017-08-14 16:59:42','2017-08-14 21:59:42',0);
+/*!40000 ALTER TABLE `rehabber` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-08-29 10:39:10
