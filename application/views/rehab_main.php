d193c5fad98bdacb04531cc2f5549ed7cb95881d
Ryan Frank <ryan_w_frank@mac.com>
2018-04-08 12:56:11 -0400

    Updated NAVBar to be responsive.

--- application/views/rehab_main.php
+++ application/views/rehab_main.php
@@ -12,19 +12,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Welcome to NC Wild Life Rehab</title>
-    <link rel="stylesheet" type="text/css" href="<?php echo site_url('application/bootstrap-4.0.0-beta-dist/css/bootstrap.min.css');?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="<?php echo site_url('application/bootstrap-4.0.0-beta-dist/css/bootstrap-datetimepicker.css');?>">
     <link rel="stylesheet" type="text/css" href="<?php echo site_url('application/css/fullcalendar.css');?>">
     <script type="text/javascript" src="<?php echo base_url('application/js/jquery-3.3.1.min.js');?>"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="<?php echo base_url('application/bootstrap-4.0.0-beta-dist/js/bootstrap.js');?>"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script type="text/javascript" src="<?php echo base_url('application/bootstrap-4.0.0-beta-dist/js/bootstrap-datetimepicker.js');?>"></script>
     <script type="text/javascript" src="<?php echo base_url('application/js/moment.js');?>"></script>
     <script type="text/javascript" src="<?php echo base_url('application/js/fullcalendar.js');?>"></script>
     <link rel="stylesheet" type="text/css" href="<?php echo site_url('application/css/css/font-awesome.css');?>">
-    <link rel="stylesheet" type="text/css" href="<?php echo site_url('application/css/fullcalendar.css');?>">
+    <!-- <link rel="stylesheet" type="text/css" href="<?php echo site_url('application/css/fullcalendar.css');?>"> -->
     <link rel="stylesheet" type="text/css" href="<?php echo site_url('application/css/ncwl.css');?>">
 
 </head>
--- application/views/rehab_menu.php
+++ application/views/rehab_menu.php
@@ -6,15 +6,18 @@
  * Time: 1:58 PM
  */
 ?>
-<div class="row ml-0 mr-0">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light col-12">
-        <a class="navbar-brand">NC Wildlife Rehab</a>
-        <div class="collapse navbar-collapse" id="ncwlNavigation">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="javascript:void(0)" onclick="window.location.reload(false);">Home <span class="sr-only">(Current)</span></a>
-                </li>
-                <?php if ( $this->ion_auth->in_group('volunteer') ){ ?>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">NC WildLife Rehab</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ncwlrMenu" aria-controls="ncwlrMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="ncwlrMenu">
+        <ul class="nav navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+                <a class="nav-link" href="javascript:void(0)" onclick="window.location.reload(false);">Home <span class="sr-only">(Current)</span></a>
+            </li>
+            <?php if ( $this->ion_auth->in_group('volunteer') ){ ?>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="navbarIntakeDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Activity / Intake</a>
                     <div class="dropdown-menu" aria-labelledby="navbarIntakeDropdown">
@@ -22,49 +25,48 @@
                         <a class="dropdown-item" href="javascript:void(0)" onclick="$(content).load('Intake/intake_animal')">Intake Animal</a>
                     </div>
                 </li>
-                <?php } ?>
+            <?php } ?>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarIntakeDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Information</a>
+                <div class="dropdown-menu" aria-labelledby="navbarIntakeDropdown">
+                    <a class="dropdown-item" href="javascript:void(0);" onclick="$(content).load('Information/donations')">Donations</a>
+                    <a class="dropdown-item" href="javascript:void(0)" onclick="$(content).load('Volunteer/schedule/event')">Events / Appearances</a>
+                </div>
+            </li>
+            <?php if ( $this->ion_auth->in_group('volunteer') ){ ?>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="navbarIntakeDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Information</a>
+                    <a class="nav-link dropdown-toggle" id="navbarIntakeDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Volunteer</a>
                     <div class="dropdown-menu" aria-labelledby="navbarIntakeDropdown">
-                        <a class="dropdown-item" href="javascript:void(0);" onclick="$(content).load('Information/donations')">Donations</a>
-                        <a class="dropdown-item" href="javascript:void(0)" onclick="$(content).load('Volunteer/schedule/event')">Events / Appearances</a>
+                        <a class="dropdown-item" href="javascript:void(0)" onclick="$(content).load('Volunteer/schedule/volunteer')">Volunteer Calendar</a>
+                        <a class="dropdown-item" href="javascript:void(0)" onclick="$(content).load('Rehab/locateRehabber')">Find Rehabber</a>
                     </div>
                 </li>
-                <?php if ( $this->ion_auth->in_group('volunteer') ){ ?>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="navbarIntakeDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Volunteer</a>
-                        <div class="dropdown-menu" aria-labelledby="navbarIntakeDropdown">
-                            <a class="dropdown-item" href="javascript:void(0)" onclick="$(content).load('Volunteer/schedule/volunteer')">Volunteer Calendar</a>
-                            <a class="dropdown-item" href="javascript:void(0)" onclick="$(content).load('Rehab/locateRehabber')">Find Rehabber</a>
-                        </div>
-                    </li>
-                <?php } ?>
-                <?php if ( $this->ion_auth->logged_in() ){ ?>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="navbarIntakeDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Charts</a>
-                        <div class="dropdown-menu" aria-labelledby="navbarIntakeDropdown">
-                            <a class="dropdown-item" href="javascript:void(0)" onclick="$(content).load('Charts/chart/Squirrel')">Squirrel Feeding Chart</a>
-                            <a class="dropdown-item" href="javascript:void(0)" onclick="$(content).load('Charts/chart/Opossum')">Opossum Feeding Chart</a>
-                            <a class="dropdown-item" href="javascript:void(0)" onclick="$(content).load('Charts/chart/Bunny')">Bunny Feeding Chart</a>
-                        </div>
-                    </li>
-                <?php } ?>
-                <?php if ($this->ion_auth->is_admin()){ ?>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="navbarIntakeDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Administration</a>
-                        <div class="dropdown-menu" aria-labelledby="navbarIntakeDropdown">
-                            <a class="dropdown-item" href="javascript:void(0)" onclick="$(content).load('Admin/add_rehabber')">Add Rehabber</a>
-                            <a class="dropdown-item" href="#">Add Locations</a>
-                            <a class="dropdown-item" href="#">Add Cages</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="javascript:void(0)" onclick="$(content).load('Admin/updateUser')">Update User</a>
-                            <a class="dropdown-item" href="#">Delete User</a>
-                            <a class="dropdown-item" href="#">Change User Password</a>
-                            <div class="dropdown-divider"></div>
-                        </div>
-                    </li>
-                <?php } ?>
-            </ul>
-        </div>
-    </nav>
-</div>
+            <?php } ?>
+            <?php if ( $this->ion_auth->logged_in() ){ ?>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarIntakeDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Charts</a>
+                    <div class="dropdown-menu" aria-labelledby="navbarIntakeDropdown">
+                        <a class="dropdown-item" href="javascript:void(0)" onclick="$(content).load('Charts/chart/Squirrel')">Squirrel Feeding Chart</a>
+                        <a class="dropdown-item" href="javascript:void(0)" onclick="$(content).load('Charts/chart/Opossum')">Opossum Feeding Chart</a>
+                        <a class="dropdown-item" href="javascript:void(0)" onclick="$(content).load('Charts/chart/Bunny')">Bunny Feeding Chart</a>
+                    </div>
+                </li>
+            <?php } ?>
+            <?php if ($this->ion_auth->is_admin()){ ?>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarIntakeDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Administration</a>
+                    <div class="dropdown-menu" aria-labelledby="navbarIntakeDropdown">
+                        <a class="dropdown-item" href="javascript:void(0)" onclick="$(content).load('Admin/add_rehabber')">Add Rehabber</a>
+                        <a class="dropdown-item" href="#">Add Locations</a>
+                        <a class="dropdown-item" href="#">Add Cages</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="javascript:void(0)" onclick="$(content).load('Admin/updateUser')">Update User</a>
+                        <a class="dropdown-item" href="#">Delete User</a>
+                        <a class="dropdown-item" href="#">Change User Password</a>
+                        <div class="dropdown-divider"></div>
+                    </div>
+                </li>
+            <?php } ?>
+        </ul>
+    </div>
+</nav>
