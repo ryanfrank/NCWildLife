1aa462615b8827a542946f2a5f13bfb6a420e4c7
Ryan Frank <ryan_w_frank@mac.com>
2018-04-08 17:15:29 -0400

    Added forums

--- application/views/forum_view.php
+++ application/views/forum_view.php
@@ -14,6 +14,6 @@
 <div class="container-fluid">
     <div class="row col-12">
         <iframe src='<?php echo $forum; ?>' style='width: 100%;height: 100%; padding: 0px; margin: 0;
-        border: none; display: block; overflow: hidden; ' onload='resizeIframe(this)'></iframe>
+        border: none; display: block; overflow: hidden;' onload='resizeIframe(this)'></iframe>
     </div>
 </div>
