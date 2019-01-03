ce21575f529a61a2bb1411651d524ce29a82a893
Ryan Frank <ryan_w_frank@mac.com>
2019-01-03 13:40:15 -0500

    damn a class in dropdown did not have href... ugh.

--- application/views/rehab_menu.php
+++ application/views/rehab_menu.php
@@ -6,36 +6,41 @@
  * Time: 1:58 PM
  */
 ?>
-
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">NC WildLife Rehab</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ncwlrMenu" aria-controls="ncwlrMenu" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
+
     <div class="collapse navbar-collapse" id="ncwlrMenu">
-        <ul class="nav navbar-nav mr-auto">
+        <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="javascript:void(0)" onclick="window.location.reload(false);">Home <span class="sr-only">(Current)</span></a>
+                <a class="nav-link" href="javascript:void(0)" onclick="window.location.reload(false);">Home <span class="sr-only">(current)</span></a>
             </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="informationDrop" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Information</a>
+                <div class="dropdown-menu" aria-labelledby="informationDrop">
+                    <a class="dropdown-item" href="javascript:void(0);" onclick="$(content).load('Information/donations')">Donations</a>
+                    <a class="dropdown-item" href="javascript:void(0)" onclick="$(content).load('Volunteer/schedule/event')">Events / Appearances</a>
+                </div>
+            </li>
+            <?php if ( !$this->ion_auth->logged_in() ){ ?>
+                <li class="nav-item active">
+                    <a class="nav-link" href="javascript:void(0)" onclick="$(content).load('Rehab/generalRehabber')">Rehabber List</a>
+                </li>
+            <?php } ?>
             <?php if ( $this->ion_auth->in_group('volunteer') ){ ?>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="javascript:void(0)" id="navbarIntakeDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Activity / Intake</a>
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarIntakeDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Activity / Intake</a>
                     <div class="dropdown-menu" aria-labelledby="navbarIntakeDropdown">
                         <a class="dropdown-item" href="javascript:void(0);" onclick="$(content).load('Intake/add_good_samaritan')">Add Good Samaritan</a>
                         <a class="dropdown-item" href="javascript:void(0)" onclick="$(content).load('Intake/intake_animal')">Intake Animal</a>
                     </div>
                 </li>
             <?php } ?>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="javascript:void(0)" id="informationDrop" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Information</a>
-                <div class="dropdown-menu" aria-labelledby="informationDrop">
-                    <a class="dropdown-item" href="javascript:void(0);" onclick="$(content).load('Information/donations')">Donations</a>
-                    <a class="dropdown-item" href="javascript:void(0)" onclick="$(content).load('Volunteer/schedule/event')">Events / Appearances</a>
-                </div>
-            </li>
             <?php if ( $this->ion_auth->in_group('volunteer') ){ ?>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="javascript:void(0)" id="volunteerDrop" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Volunteer</a>
+                    <a class="nav-link dropdown-toggle" href="#" id="volunteerDrop" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Volunteer</a>
                     <div class="dropdown-menu" aria-labelledby="volunteerDrop">
                         <a class="dropdown-item" href="javascript:void(0)" onclick="$(content).load('Volunteer/schedule/volunteer')">Volunteer Calendar</a>
                         <a class="dropdown-item" href="javascript:void(0)" onclick="$(content).load('Volunteer/vendorView')">Vendor Information</a>
@@ -46,7 +51,7 @@
             <?php } ?>
             <?php if ( $this->ion_auth->logged_in() ){ ?>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="javascript:void(0)" id="chartsDrop" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Charts</a>
+                    <a class="nav-link dropdown-toggle" href="#" id="chartsDrop" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Charts</a>
                     <div class="dropdown-menu" aria-labelledby="chartsDrop">
                         <a class="dropdown-item" href="javascript:void(0)" onclick="$(content).load('Charts/chart/Squirrel')">Squirrel Feeding Chart</a>
                         <a class="dropdown-item" href="javascript:void(0)" onclick="$(content).load('Charts/chart/Opossum')">Opossum Feeding Chart</a>
@@ -56,7 +61,7 @@
             <?php } ?>
             <?php if ($this->ion_auth->is_admin()){ ?>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="javascript:void(0)" id="adminDrop" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Administration</a>
+                    <a class="nav-link dropdown-toggle" href="#" id="adminDrop" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Administration</a>
                     <div class="dropdown-menu" aria-labelledby="adminDrop">
                         <a class="dropdown-item" href="javascript:void(0)" onclick="$(content).load('Admin/add_rehabber')">Add Rehabber</a>
                         <a class="dropdown-item" href="#">Add Locations</a>
@@ -67,11 +72,6 @@
                         <a class="dropdown-item" href="#">Change User Password</a>
                         <div class="dropdown-divider"></div>
                     </div>
-                </li>
-            <?php } ?>
-            <?php if ( !$this->ion_auth->logged_in() ){ ?>
-                <li class="nav-item active">
-                    <a class="nav-link" href="javascript:void(0)" onclick="$(content).load('Rehab/generalRehabber')">Rehabber List</a>
                 </li>
             <?php } ?>
         </ul>
