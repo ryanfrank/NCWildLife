926a4d86462d5900377cfc206806c76e1caee16e
Ryan Frank <ryan_w_frank@mac.com>
2019-01-03 13:31:40 -0500

    found a bunch of ID issues in menu... fixed.

--- application/views/rehab_menu.php
+++ application/views/rehab_menu.php
@@ -18,18 +18,11 @@
             <li class="nav-item active">
                 <a class="nav-link" href="javascript:void(0)" onclick="window.location.reload(false);">Home <span class="sr-only">(current)</span></a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                <a class="nav-link dropdown-toggle" href="#" id="informationDrop" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Information</a>
+                <div class="dropdown-menu" aria-labelledby="informationDrop">
+                    <a class="dropdown-item" href="javascript:void(0);" onclick="$(content).load('Information/donations')">Donations</a>
+                    <a class="dropdown-item" href="javascript:void(0)" onclick="$(content).load('Volunteer/schedule/event')">Events / Appearances</a>
                 </div>
             </li>
             <li class="nav-item">
